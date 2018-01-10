30fe344ce2ffb20412bfcf69912cb4bed40b9b11
khalid abubakr <khalid7.bkr@gmail.com>
2018-01-10 22:44:14 +0200

    fixed some bugs in item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,19 @@
 class Item < ApplicationRecord
+
+
+  validates :title , presence: true
+  validates :price , presence: true, length: { minimum: 0 }
+  validates :type , presence: true
+  validates :description , presence: true
+
+
+   enum type: {
+    kits: 0,
+    mods: 1,
+    tanks: 2,
+    jucies: 3,
+    accessories: 4,
+    combo: 5
+  }
+
 end
