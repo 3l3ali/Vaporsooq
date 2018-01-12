0856cd4fe985cd957ef5ecbcbd037c1ba7caef66
Mohammad Alali <m.a.alsyouf@gmail.com>
2018-01-12 21:57:13 +0200

    configered carrierwave and cloudinary

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180110211605) do
+ActiveRecord::Schema.define(version: 20180112193650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20180110211605) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "photo"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
@@ -47,6 +48,7 @@ ActiveRecord::Schema.define(version: 20180110211605) do
     t.string "first_name"
     t.string "last_name"
     t.integer "user_type", default: 0
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
