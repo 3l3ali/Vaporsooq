048a7f2b372995717bc5bb9846fd384486a625a6
khalid abubakr <khalid7.bkr@gmail.com>
2018-01-12 21:25:00 +0200

    created items controller and added the new/create/edit/update/destroy list

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,48 @@
+class ItemsController < ApplicationController
+  before_action :find_user, only: [:show]
+  before_action :set_item, only: [:show, :new, :edit, :update, :destroy]
+
+  def show          # GET /items/:id
+  end
+
+  def new           # GET /items/new
+    @item = Item.new
+  end
+
+  def create        # POST /items
+    @item = Item.new(item_params)
+    @item.user = current_user
+    if @item.save
+      redirect_to user_item_path(@item)
+    else
+      render :new
+    end
+  end
+
+  def edit          # GET /items/:id/edit
+  end
+
+  def update        # PATCH /items/:id
+    @item.update(item_params)
+    redirect_to user_item_path(@item)
+  end
+
+  def destroy       # DELETE /items/:id
+    @item.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def find_user
+    @user = User.find(params[:user_id])
+  end
+
+  def item_params
+    params.require(:item).permit(:title, :description, :price, :item_type)
+  end
+end
