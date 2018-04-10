e2a1656adf9be9b71e4cf4c4b278ec0c5515284e
Mohammad Alali <m.a.alsyouf@gmail.com>
2018-04-11 02:39:19 +0300

    href logic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @nader = User.find_by(email: "nader@seller.com")
+    @rami = User.find_by(email: "rami@seller.com")
+    @rashed = User.find_by(email: "rashed@seller.com")
+
   end
   def explore
     @q = Item.ransack(params[:q])
