ee893ba4ec221380fa8de460b26f79e9a9c3c411
Mohammad Alali <m.a.alsyouf@gmail.com>
2018-04-11 04:02:27 +0300

    uncommented the home links back

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    # @nader = User.find_by(email: "nader@seller.com")
-    # @rami = User.find_by(email: "rami@seller.com")
-    # @rashed = User.find_by(email: "rashed@seller.com")
+    @nader = User.find_by(email: "nader@seller.com")
+    @rami = User.find_by(email: "rami@seller.com")
+    @rashed = User.find_by(email: "rashed@seller.com")
 
   end
   def explore
