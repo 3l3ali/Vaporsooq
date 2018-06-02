e2f8cc36d07908e4cb91e052398a25a0101db63e
Mohammad Alali <m.a.alsyouf@gmail.com>
2018-06-02 22:03:08 +0300

    profile item type filter added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @items = @user.items.order(updated_at: :desc)
+    # @items = @user.items.order(updated_at: :desc)
+    if params[:item_type].present?
+      @items = @user.items.where(item_type: params[:item_type]).order(updated_at: :desc)
+    else
+      @items = @user.items.order(updated_at: :desc)
+    end
   end
 end
